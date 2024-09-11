65acc77ea6795ace6b93c7f13c4bc266daf6f9d4
Valentín Martínez <valentecreativo@proton.me>
2024-09-11 17:15:13 -0600

    Conexión con Minato Testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as fs from "fs";
 
+const { PrivateKey } = JSON.parse(fs.readFileSync('./secret.json').toString());
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "minato",
+  networks: {
+    minato: {
+      url: "https://rpc.minato.soneium.org/",
+      chainId: 1946,
+      accounts: [PrivateKey]
+    }
+  }
 };
 
 export default config;
